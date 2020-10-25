cddaafd31a152a2e96f7509b98934956ab5b9b6b
Alla-Ts <aecybko@gmail.com>
2020-10-25 17:20:03 +0300

    init: start and finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let numstring = n.toString();
+    let splitString = numstring.split('');
+    let reverseArray = splitString.reverse();
+    let num = parseFloat(reverseArray.join('')) * Math.sign(n);
+    let numReverse = Math.abs(num);
+
+
+    return numReverse;
+
+
+    // return (
+    //     parseFloat(
+    //         n
+    //         .toString()
+    //         .split('')
+    //         .reverse()
+    //         .join('')
+    //     ) * Math.sign(n)
+    // )
 }
